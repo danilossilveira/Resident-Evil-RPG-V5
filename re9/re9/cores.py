fd0e24198d1d7ef1d2b50806ac235ade77906a3c
Danilo <danilosantossilveira.com@gmail.com>
2026-02-26 10:22:47 -0300

    Delete re9/re9 directory

--- re9/re9/cores.py
+++
@@ -1,9 +0,0 @@
-class Cores:
-    VERMELHO = '\033[91m'
-    VERDE = '\033[92m'
-    AMARELO = '\033[93m'
-    AZUL = '\033[94m'
-    CIANO = '\033[96m'
-    BRANCO = '\033[97m'
-    AZUL_CLARO = "\033[1;36m"
-    RESET = '\033[0m'
